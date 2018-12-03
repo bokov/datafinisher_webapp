b21ddd5ba494097c9504c8be148a79ef9102a1c8
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-03 17:53:04 -0600

    First commit

---
+++ df_reticulate.R
@@ -0,0 +1,11 @@
+# R quick and dirty so I don't forget what I figured out today
+
+
+library(reticulate)
+# THIS is what fixes that stupid "no module named ..." message
+# NOTE: this relies on there being most of datafinisher being 
+# copied/linked over to the same director as this.
+py$sys$path <- c(py$sys$path,getwd())
+source_python('df_reticulate.py')
+
+# afterward, can access objects via py$FOO
