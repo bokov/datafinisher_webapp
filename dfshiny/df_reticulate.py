f37b484e3eccf4829974f9e0cc4cca95651b7405
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-04 15:51:30 -0600

    df_reticulate.py;bugfix;Stupid argparse keeps insinuating itself when trying to import from files non-interactively, trying to decouple.

--- dfshiny/df_reticulate.py
+++ dfshiny/df_reticulate.py
@@ -1,13 +1,22 @@
-import csv,json,re #,ast (for literal_eval, not yet used)
-from os import path
+
+class EmptyParser():
+    def parse_args():
+        return
+      
+import argparse,sys,os,csv,json,re #,ast (for literal_eval, not yet used)
+#from os import path
+
+sys.path+=[os.getcwd()+'/datafinisher']
+
 from df_fn import DFMeta,DFCol,autosuggestor
 from rules import rules2
 # import pandas as pd (for DataFrame, not yet used)
 
+argparse.ArgumentParser = EmptyParser
+
 # to allow ridiculously large cells
 csv.field_size_limit(100000000)
 
-
-def rdfmeta(myheader,rawmeta=None,rules=rules2,suggestions=autosuggestor,**kwargs):
-	      out = DFMeta(myheader,rawmeta,rules,suggestions=suggestions,**kwargs)
-	      return out.getHeaders(bycol=True)
+#def rdfmeta(myheader,rawmeta=None,rules=rules2,suggestions=autosuggestor,**kwargs):
+#      out = DFMeta(myheader,rawmeta,rules,suggestions=suggestions,**kwargs)
+#      return out.getHeaders(bycol=True)
