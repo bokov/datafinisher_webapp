4645f62e80423c2e5d35177a0b813a993f8eb101
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-05 17:32:26 -0600

    dfshiny;functionality;Made the 'Transform Data' tab collapsible. Beat my head against some kind of graceful drag and drop solution to selecting from among eligible rules to create output column specifications but knowing that soon there will be rules which can be used to generate several columns by changing the argument (and are not valid without an argument). Giving up on that and going back to checkboxGroupInput(). Also tried to use bs_accordion() to maybe at least only have to worry about one input column at a time, but cannot pass the custom javascript event notification back to server (https://stackoverflow.com/questions/53642157/shiny-how-to-detect-which-accordion-elements-is-selected). The revised plan is to just use actionButtons to add to a list, indexed by output column name. If the name already exists it gets overwritten, if it's new it gets added. If a rule requires an argument the actionBox is disabled until the argument is given. Not sure how the performance will be though.

--- dfshiny/server.R
+++ dfshiny/server.R
@@ -1,4 +1,5 @@
-library(shiny); library(reticulate); library(readr);
+library(shiny); library(shinyjqui); library(bsplus);
+library(reticulate); library(readr);
 
 colInfoBox <- function(incolname,incols){
   #' incols:    reactivevalues sub-object that is a python DFCol
@@ -28,7 +29,14 @@ shinyServer(function(input, output, session) {
   #py$sys$path <- c(py$sys$path,paste0(getwd(),'/datafinisher'));
   source_python('df_reticulate.py');
   
-  rv <- reactiveValues(uitest=textInput('foo','Bar'));
+  rv <- reactiveValues(uitest=div(
+     orderInput('source', 'Source'
+                ,items = factor(sample(month.abb,15,rep=T))
+                ,as_source = TRUE, connect = 'dest')
+    ,orderInput('dest', 'Dest', items = NULL
+               , placeholder = 'Drag items here...')
+    #verbatimTextOutput('order'))
+    ));
   
   rvp <- reactiveValues();
   
@@ -59,9 +67,18 @@ shinyServer(function(input, output, session) {
     # Now we have a sample data-file!
     rvp$dfmeta <- py$DFMeta(names(dat),as.character(dat[1,])
                             ,suggestions=py$autosuggestor);
-    # create current test output
-    rv$ui_transform <- lapply(rvp$dfmeta$inhead
-                        ,colInfoBox,rvp$dfmeta$incols);
+    # create column controls
+    infodivs <- bs_accordion('infodivs');
+    for(ii in rvp$dfmeta$inhead){
+      infodivs <- bs_append(infodivs,ii
+                            ,colInfoBox(ii,rvp$dfmeta$incols));
+    }
+    rv$ui_transform <- div(infodivs,id=infodivs_parent,onclick="
+function(){ Shiny.onInputChange('selectedinfodiv',document.getElementsByClassName('in')[0].getElementsByClassName('shiny-input-container')[0].id)}
+                           ");
+    # infodivs <- sapply(rvp$dfmeta$inhead
+    #                     ,colInfoBox,rvp$dfmeta$incols
+    #                     ,simplify = F);
     # return a preview of the input
     return(head(dat[-1,],200));
   });
@@ -70,7 +87,49 @@ shinyServer(function(input, output, session) {
   
   observeEvent(input$debug,{
     req(rvp$dfmeta);
+    # rv$tv <- list()
+    # rv$tv$testname <- rvp$dfmeta$inhead[42];
+    # rv$tv$testcol <- rvp$dfmeta$incols[[rv$tv$testname]];
+    # rv$tv$testrules <- rv$tv$testcol$rules; 
+    # rv$tv$testrulesinfo <- sapply(
+    #   names(rv$tv$testrules), function(xx) {
+    #     with(rv$tv$testrules[[xx]]
+    #          ,list(id=sprintf(gsub('^\\{0\\}','%s'
+    #                                ,extractors[[1]][2])
+    #                           ,rv$tv$testname)
+    #                ,descr=ruledesc,suggested=suggested))
+    #     },simplify=F)
+    # rv$tv$testrulesdivs <- lapply(
+    #   rv$tv$testrulesinfo,function(xx) {
+    #     with(xx
+    #          ,jqui_draggable(div(id=paste0('cb_',id),id,br()
+    #                              ,span(descr,class='annotation')
+    #                              ,class='btn btn-default')
+    #                          ,options=list(scope='dest')))
+    #     });
+    # 
+    # rv$tv$workingui <- div(
+    #   orderInput('source', 'Source'
+    #              ,items = sample(month.abb,15,rep=T)
+    #              ,as_source = TRUE, connect = 'dest')
+    #   ,orderInput('dest', 'Dest', items = NULL
+    #               , placeholder = 'Drag items here...'));
+      
     browser();
+    # rv$uitest <- with(rv$tv
+    #                   ,div(
+    #   # orderInput('source', 'Source'
+    #   #            ,items = testrulesdivs
+    #   #            ,as_source = TRUE, connect = 'dest')
+    #   div(testrulesdivs,id='FOO')
+    #   # ,jqui_droppable(div('Drag items here...')
+    #   #                 ,options=list(scope='dest'))
+    #   ,jqui_sortable(div('Drag items here...',id='dest')
+    #                  ,options=list(scope='dest'))
+    #   # ,orderInput('dest', 'Dest', items = NULL
+    #   #             , placeholder = 'Drag items here...')
+    #   )
+    #   );
   });
   
   output$test <- renderUI({rv$uitest});
--- dfshiny/ui.R
+++ dfshiny/ui.R
@@ -5,11 +5,13 @@ options(shiny.maxRequestSize=50*1024^2);
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
+  useShinyjs()
+  
   # Application title
-  titlePanel("Datafinisher WebApp"),
+  ,titlePanel("Datafinisher WebApp")
   
   # Sidebar 
-  sidebarLayout(
+  ,sidebarLayout(
     
     sidebarPanel(
       
@@ -21,15 +23,21 @@ shinyUI(fluidPage(
                             ,"text/comma-separated-values,text/plain"
                             ))
       ,actionButton('debug','Debug')
-      ),
+      )
 
     # Preview
-    mainPanel(
+    ,mainPanel(
       tabsetPanel(
          tabPanel('Debug',uiOutput('test'))
-        ,tabPanel('Transform Data',uiOutput('tb_transform'))
+        ,tabPanel('Transform Data'
+                  ,uiOutput('tb_transform'))
         ,tabPanel('Input Data',dataTableOutput('tb_infile_prev'))
         )
       )
     )
+#   ,tags$script("
+# document.getElementById('infodivs').onclick = function(){
+# Shiny.onInputChange('selectedinfodiv',document.getElementsByClassName('in')[0].getElementsByClassName('shiny-input-container')[0].id)
+# }
+#               ")
 ))
