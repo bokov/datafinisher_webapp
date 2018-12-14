9d875ca6683405975bf5c52f481d17980422d3a0
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-14 17:54:56 -0600

    templates.R;organization;Some of the HTML blocks being rendered will now be via templates.

---
+++ dfshiny/templates.R
@@ -0,0 +1,18 @@
+templates <- list(
+ divavailable = '<div>{{ own_name }}
+<button class="btn btn-default action-button btn btn-success" id="{{ addbid }}" type="button">Add/Update</button>
+  <br/>
+  <span class="annotation">{{ ruledesc }}</span>
+    </div>'
+,multidivavailable = '<div id={{ divIDavilable }}>{{ innerDivs }}</div>'
+,incolui='<div id="{{ divIDchosen }}"><b>Chosen:</b></div>
+<div id="{{ divIDavailable }}"><b>Available:</b>{{ divavailable }}</div>
+'
+,divfull ='<div id="{{ incolid }}">{{ incolid }}<br/>
+  <span class="annotation">{{ incoldesc }}</span>
+  {{ incolui }}
+</div>
+'
+);
+
+c()
