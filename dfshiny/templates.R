d1544b5a9757b8ceeef924c62c9fbc0e1fc46d8b
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-14 23:18:22 -0600

    templates.R;cleanup;Moved filterlist to this file

--- dfshiny/templates.R
+++ dfshiny/templates.R
@@ -19,4 +19,42 @@ templates <- list(
 '
 );
 
+filterlist <- list(
+  concept_cd=list(name = 'cc'
+                  , type = 'string'
+                  , input = 'selectize'
+                  , label = 'Concept Code (concept_cd)'
+                  , values=c()
+                  , criteria = quote(isTRUE(ccd > 1)))
+  ,modifier_cd=list(name = 'mc', type = 'string', input = 'text'
+                    ,label='Modifier Code (modifier_cd)'
+                    ,criteria=quote(isTRUE(mod > 0)))
+  ,instance_num=list(name='ix',type='integer'
+                     ,label='Instance Number (instance_num)'
+                     ,criteria=quote(isTRUE(mxinsts>1)))
+  ,valtype_cd=list(name = 'vt', type = 'string', input = 'text'
+                   ,label='Value Type (valtype_cd)'
+                   ,criteria=quote(isTRUE(valtype_cd>0)))
+  ,tval_char=list(name = 'tc', type = 'string', input = 'text'
+                  ,label='Text Val (tval_char)'
+                  ,criteria=quote(isTRUE(tval_char>0)))
+  ,nval_num=list(name='nv',type='double'
+                 ,label='Numeric Value (nval_num)'
+                 ,criteria=quote(isTRUE(nval_num>0)))
+  ,valueflag_cd=list(name = 'vf', type = 'string', input = 'text'
+                     ,label='Outside Reference Range (valueflag_cd)'
+                     ,criteria=quote(isTRUE(valueflag_cd>0)))
+  ,quantity_num=list(name='qt',type='double'
+                     ,label='Quantity (quantity_num)'
+                     ,criteria=quote(isTRUE(quantity_num>0)))
+  ,units_cd=list(name = 'un', type = 'string', input = 'text'
+                 ,label='Unit of Measure (units_cd)'
+                 ,criteria=quote(isTRUE(units_cd>0)))
+  ,location_cd=list(name = 'lc', type = 'string', input = 'text'
+                    ,label='Location Code (location_cd)'
+                    ,criteria=quote(isTRUE(location_cd>0)))
+  ,confidence_num=list(name='cf',type='double'
+                       ,label='Confidence Level (confidence_num)'
+                       ,criteria=quote(isTRUE(confidence_num>0))));
+
 c()
