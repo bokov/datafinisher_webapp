079fbbb56059906523883dfd3ba13cfa288454c7
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-14 19:34:30 -0600

    templates.R;functionality;Now the divavailable template accommodates an xxsel element which is a selectize widget for available rules whose split_by_code is true

--- dfshiny/templates.R
+++ dfshiny/templates.R
@@ -1,13 +1,16 @@
 templates <- list(
- divavailable = '<div>{{own_name}}
-<button class="btn btn-default action-button btn btn-success" id={{addbid}} type="button">Add/Update</button>
-  <br/>
+
+ divavailable = '<div class="available-transform">{{own_name}}
+<button class="btn action-button btn-success" id={{addbid}} type="button">Add/Update</button>{{xxsel}}<br/>
   <span class="annotation">{{ruledesc}}</span>
     </div>'
+ 
 ,multidivavailable = '<div id={{divIDavailable}}>{{ innerDivs }}</div>'
+
 ,incolui='<div id={{divIDchosen}}><b>Chosen:</b></div>
 <div id={{divIDavailable}}><b>Available:</b>{{divavailable}}</div>
 '
+
 ,divfull ='<div id={{incolid}}>{{ incolid }}<br/>
   <span class="annotation">{{incoldesc}}</span>
   {{incolui}}
