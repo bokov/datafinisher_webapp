4657b235c82ce55405d9463ed1910190598daf26
Alex F. Bokov <bokov@uthscsa.edu>
2018-12-14 18:22:11 -0600

    templates.R;bugfix;Do NOT put spaces between the double-brackets and the expressions they contain. Screws up the IDs and jQuery can't find them then

--- dfshiny/templates.R
+++ dfshiny/templates.R
@@ -1,16 +1,16 @@
 templates <- list(
- divavailable = '<div>{{ own_name }}
-<button class="btn btn-default action-button btn btn-success" id="{{ addbid }}" type="button">Add/Update</button>
+ divavailable = '<div>{{own_name}}
+<button class="btn btn-default action-button btn btn-success" id={{addbid}} type="button">Add/Update</button>
   <br/>
-  <span class="annotation">{{ ruledesc }}</span>
+  <span class="annotation">{{ruledesc}}</span>
     </div>'
-,multidivavailable = '<div id={{ divIDavilable }}>{{ innerDivs }}</div>'
-,incolui='<div id="{{ divIDchosen }}"><b>Chosen:</b></div>
-<div id="{{ divIDavailable }}"><b>Available:</b>{{ divavailable }}</div>
+,multidivavailable = '<div id={{divIDavailable}}>{{ innerDivs }}</div>'
+,incolui='<div id={{divIDchosen}}><b>Chosen:</b></div>
+<div id={{divIDavailable}}><b>Available:</b>{{divavailable}}</div>
 '
-,divfull ='<div id="{{ incolid }}">{{ incolid }}<br/>
-  <span class="annotation">{{ incoldesc }}</span>
-  {{ incolui }}
+,divfull ='<div id={{incolid}}>{{ incolid }}<br/>
+  <span class="annotation">{{incoldesc}}</span>
+  {{incolui}}
 </div>
 '
 );
