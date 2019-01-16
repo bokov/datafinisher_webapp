c8a2afdaa9914e476ad92c0d0d84212b5c164e11
Alex Bokov <bokov@uthscsa.edu>
2019-01-16 17:27:43 -0600

    ui.R;usability;Added support for shinyhelper

--- dfshiny/ui.R
+++ dfshiny/ui.R
@@ -44,6 +44,36 @@ instance with data that contains HIPAA identifiers (there are no geniuine
 identifiers in the demo data provided here)")
 );
 
+help_upload <- "
+Please upload a .csv file that has been created by this version of DataFinisher
+or a .db SQLite file created by DataBuilder. Not any .csv or .db file will work:
+they have to have been created by DataFinisher or DataBuilder. Excel files are 
+not currently supported.";
+
+help_tinput <- "This is a sample of the the top few rows from the file you 
+uploaded.";
+
+help_ttransform <- "Here you can change which transformations are applied to
+which main columns. One main column can be used to created output columns. If 
+you don't choose anything for a column, it will not be deleted. Rather, default
+transformations will be applied. The main columns always keep getting passed on
+with all their metadata so if necessary you can re-upload a file produced by 
+DataFinisher and make a different set of choices.";
+
+help_tcustomize <- "If none of the built-in transformations do what you need them
+to do, you can create your own in this tab and specify for which columns it 
+be available.";
+
+help_toutput <- "When you want to see how your transformations will look, click 
+the 'Generate/Update Preview of Results' button. Once it completes, you will get
+access to the 'Prepare Results for Download Button'. After you click that and 
+the download is prepared (this may take a while if your file is large) you will
+see a third button, 'Download Full Results'. This tab does not update 
+automatically. You will need to manually click 'Generate/Update Preview of 
+Results' whenever you want to see the most recent changes you made.";
+
+hcol <- '#008c99'
+
 shinyUI(fluidPage(
   tags$head(tags$link(rel="shortcut icon", href="favicon.ico"))
   ,includeCSS('df.css')
@@ -51,7 +81,9 @@ shinyUI(fluidPage(
   ,fluidRow(
      column(1,img(src='sitelogo_color.png',width='45px'))
     ,column(2,h3("Datafinisher WebApp",id='apptitle'))
-    ,column(4,fileInput("infile", "Choose a CSV file to upload"
+    ,column(4,fileInput("infile", tagList("Choose a CSV file to upload"
+                        ,helper(strong(),type='inline',content=help_upload
+                                 ,colour=hcol))
                         ,multiple = FALSE,width = '400px'
                         ,accept = c("text/csv","text/tsv",".csv",".tsv"
                                     ,".tab",".txt",".db"
@@ -74,27 +106,36 @@ shinyUI(fluidPage(
                   $('.nav-tabs>.active>a>span')[0].id=='tInputData'",
                                    div("Reading your file, please wait..."
                                        ,id="loading_message_in"))
+                 ,helper(strong(),type='inline',content=help_tinput,colour=hcol)
                  ,dataTableOutput('tb_infile_prev'))
         ,tabPanel(span(id='tTransform','Transform',br(),'Columns')
+                  ,helper(strong(),type='inline',content=help_tinput,colour=hcol)
                   ,uiOutput('tb_transform'))
         ,tabPanel(span(id='tCustomTrans','Customize',br(),'Transforms')
+                  ,helper(strong(),type='inline',content=help_ttransform
+                          ,colour=hcol)
                   # transform name
                   ,textInput('customTrName'
                              ,'Choose a name for your transformation'
-                             ,'custom',width='80vh')
+                             ,'custom',width='80vh') %>% 
+                    helper(type='inline',content='placeholder',colour=hcol)
                   # description
                   ,textAreaInput('customTrDesc'
                                  ,'Please write a brief description of what you
                                    intend for your custom column transformation
-                                   to be (required)',width='80vh')
+                                   to be (required)',width='80vh') %>% 
+                    helper(type='inline',content='placeholder',colour=hcol)
                   # multi-select columns
-                  ,uiOutput('customWhichCols')
+                  ,uiOutput('customWhichCols') %>% 
+                    helper(type='inline',content='placeholder',colour=hcol)
                   # fields to return
-                  ,hidden(uiOutput('customWhichFields'))
+                  ,hidden(uiOutput('customWhichFields') %>% 
+                            helper(type='inline',content='placeholder',colour=hcol))
                   # aggregation
                   ,hidden(selectInput('customAggregate'
                                 ,'If multiple values exist for the same visit, how do you wish to aggregate them?',selectize = F
-                               ,choices=''))
+                               ,choices='') %>% 
+                            helper(type='inline',content='placeholder',colour=hcol))
                   # qb widget
                   ,hidden(a(id='customQBhead'
                      ,class='btn btn-default'
@@ -109,7 +150,8 @@ shinyUI(fluidPage(
                        ,id='customQB'
                        #,'Placeholder Text'
                        ,queryBuilderOutput('qbtest')
-                       )
+                       ) %>% 
+                    helper(type='inline',content='placeholder',colour=hcol)
                   # save
                   ,disabled(actionButton('customSave','Save'))
                   # cancel
@@ -121,6 +163,7 @@ shinyUI(fluidPage(
                   $('.nav-tabs>.active>a>span')[0].id=='tOutPrev'",
                                     div("Preparing your file, please wait..."
                                         ,id="loading_message_out"))
+                  ,helper(strong(),type='inline',content=help_toutput,colour=hcol)
                   ,actionButton('outprev','Generate/Update Preview of Results'
                                 ,icon=icon('eye'))
                   ,hidden(actionButton('outwrite','Prepare Results for Download'
