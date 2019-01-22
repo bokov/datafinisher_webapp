c7b22775115b0926b016e473947a4d5b598a5215
Alex Bokov <bokov@uthscsa.edu>
2019-01-22 16:05:54 -0600

    insuredeploy_inactive.R;deployability;If renamed to insuredeploy.R then will REALLY try hard toinstall all needed libraries in local directory

--- dfshiny/global.R
+++ dfshiny/global.R
@@ -1,4 +1,5 @@
 shinyapps <- file.exists('queryBuilder_0.0.2.9000.tar.gz');
+if(file.exists('insuredeploy.R')) source('insuredeploy.R');
 
 # Let me just say that Winston Chang is a genius
 # https://gist.github.com/wch/c3653fb39a00c63b33cf
