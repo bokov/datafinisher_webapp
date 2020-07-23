5a762a7fdf73ad3300f1515fd77b9b8159562b01
Alex Bokov <bokov@uthscsa.edu>
2020-07-23 11:43:14 -0500

    df_reticulate.py; feature; Find and load 'autosuggestor.json' to enable customization of suggestion rules

--- dfshiny/df_reticulate.py
+++ dfshiny/df_reticulate.py
@@ -1,10 +1,17 @@
 #import argparse,sys,os,csv,json,re #,ast (for literal_eval, not yet used)
-import sys,os,csv
+import sys,os,csv,json
 #from os import path
 
 sys.path+=[os.getcwd()+'/datafinisher']
 
 from df_fn import DFMeta,autosuggestor #,DFCol
+if os.path.isfile('autosuggestor.json'):
+      print('autosuggestor.json found in app directory')
+      autosuggestor=json.loads(''.join([xx for xx in open('autosuggestor.json').read().split('\n') if not xx.startswith('#')]))
+elif os.path.isfile('datafinisher/autosuggestor.json'):
+      print('autosuggestor.json found in datafinisher subdirectory')
+      autosuggestor=json.loads(''.join([xx for xx in open('datafinisher/autosuggestor.json').read().split('\n') if not xx.startswith('#')]))
+else: print('No custom autosuggester files found')
 #from df_fn import shortenwords,dropletters,makeTailUnq,ob2tag
 from rules import rules
 # import pandas as pd (for DataFrame, not yet used)
